8e24d19db8f5a87caa00ee9155a92f6be054c8d4
Jared Parbs <japarbs@e1z2r3p42.42.us.org>
2019-05-29 17:58:44 -0700

    Added libft and files

--- get_next_line.h
+++ get_next_line.h
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: japarbs <japarbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/27 01:32:33 by japarbs           #+#    #+#             */
-/*   Updated: 2019/05/29 17:43:42 by japarbs          ###   ########.fr       */
+/*   Created: 2019/05/28 23:43:33 by japarbs           #+#    #+#             */
+/*   Updated: 2019/05/29 17:43:39 by japarbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int get_next_line(const int fd, char **line)
-{
-	
-}
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-/*
-**	Sudo Code
-**	Start code defining starting lines
-**
-*/
+int get_next_line(const int fd, char **line);
+
+#endif
