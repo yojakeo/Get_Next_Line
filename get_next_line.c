8988738bee8e95734aa63a0f66c7a57d0bbf87d0
Jared Parbs <japarbs@e1z2r3p40.42.us.org>
2019-06-06 16:21:12 -0700

    Compiler & Norm corrections

--- get_next_line.c
+++ get_next_line.c
@@ -6,72 +6,89 @@
 /*   By: japarbs <japarbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 01:32:33 by japarbs           #+#    #+#             */
-/*   Updated: 2019/06/02 21:05:26 by japarbs          ###   ########.fr       */
+/*   Updated: 2019/06/06 16:19:32 by japarbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#define BUFFCHECK	{*(buffer) = count; break;}
+#define FREETMP	{free(*tmp); free(tmp);}
 
-char	**read_line(int fd, char **line, size_t line_index, int EOF)
+/*
+**	Reads line from FD and places buffs to tmp, Allocating the total chars
+**	needed to **line's current index. Returning tmp to be processed
+**	(Coppied from **tmp to **line) by Process_line.
+**	It is passed eof (Bollian) to signal if it is at End Of File.
+**	cbl = Current Buffer Length.
+*/
+
+char	**read_line(int fd, char **line, size_t line_index, int eof)
 {
 	size_t			count;
-	size_t			i;
+	size_t			cbl;
 	char			buffer[BUFF_SIZE];
-	char			tmp[MAX_BUFFS][BUFF_SIZE + 1];
+	char			**tmp;
 
 	count = 0;
-	i = 0;
+	cbl = 0;
+	if (!(tmp = (char **)malloc(sizeof(char *) * (MAX_BUFFS + 1))))
+		return (NULL);
 	while (read(fd, buffer, BUFF_SIZE))
 	{
-		if (!buffer[BUFF_SIZE])
-			EOF = 1;
-		i = ft_findsubstrlen(buffer, '\n');
-		count += i;
-		ft_memcpy(*(tmp), buffer, i);
-		**(++tmp);
-		if (i < BUFF_SIZE)
-		{
-			*(buffer) = count;
-			break;
-		}
+		if (!buffer[BUFF_SIZE - 1])
+			eof = 1;
+		cbl = ft_findsubstrlen(buffer, '\n');
+		if (!(*(tmp) = (char *)ft_memalloc(cbl + 1)))
+			return (NULL);
+		count += cbl;
+		ft_memcpy(*(tmp), buffer, cbl);
+		(++tmp);
+		if (cbl < BUFF_SIZE)
+			BUFFCHECK;
 	}
 	line[line_index] = ft_strnew(count);
 	return (tmp);
 }
+
 /*
-**	Copies processed line from read_line to **
+**	Copies processed line from read_line to **line at the current index.
+**	Converts the **tmp buffers to a single * for **line.
 */
+
 int		process_line(int fd, char **line, size_t line_index)
 {
-	int				EOF;
+	int				eof;
 	size_t			i;
 	char			**tmp;
-	
-	EOF = 0;
+
+	eof = 0;
 	i = 0;
-	tmp = read_line(fd, line, line_index, EOF);
-	while (**(tmp))
+	tmp = read_line(fd, line, line_index, eof);
+	while (tmp)
 	{
-		if (*(tmp) == '\n')
+		if (**(tmp) == '\n')
 		{
 			line[line_index][i] = '\0';
 			return (0);
 		}
-		line[line_index][i++] = *(tmp++);
+		line[line_index][i++] = **(tmp++);
 		if (!*(tmp))
-			**(++tmp);
+			(++tmp);
 	}
-	if (!EOF)
+	FREETMP;
+	if (!eof)
 		return (0);
 	return (1);
 }
+
 /*
 **	Main function, runs checks and keeps track of line positon & file positon
 */
+
 int		get_next_line(const int fd, char **line)
 {
 	static size_t	line_index;
-	int rval;
+	int				rval;
 
 	if (fd < 0 || !line)
 		return (-1);
@@ -83,11 +100,3 @@ int		get_next_line(const int fd, char **line)
 	line_index = 0;
 	return (1);
 }
-
-/*
-**	Sudo Code
-**	Start code defining indexes and strings. Read the file and run checks.
-**	Returns are 1: Line read, 0: Finished Reading, -1: Error.
-**	Reads one line then exits. Looping get_next_line will read multipule lines.
-**	 
-*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,18 @@
 /*   By: japarbs <japarbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 23:43:33 by japarbs           #+#    #+#             */
-/*   Updated: 2019/06/02 18:13:55 by japarbs          ###   ########.fr       */
+/*   Updated: 2019/06/06 16:19:54 by japarbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 32
-# define MAX_BUFFS 32 
+# define MAX_BUFFS 32
 
 # include "libft/libft.h"
 
-char	**read_line(int fd, char **line, size_t line_index, int EOF);
+char	**read_line(int fd, char **line, size_t line_index, int eof);
 int		process_line(int fd, char **line, size_t line_index);
 int		get_next_line(const int fd, char **line);
 
