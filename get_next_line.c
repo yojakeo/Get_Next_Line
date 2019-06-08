6fa99a508ff452a05122e7b670713233570624b7
Jared Parbs <japarbs@e1z2r3p40.42.us.org>
2019-06-07 17:40:07 -0700

    Function restructure

--- get_next_line.c
+++ get_next_line.c
@@ -6,38 +6,37 @@
 /*   By: japarbs <japarbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 01:32:33 by japarbs           #+#    #+#             */
-/*   Updated: 2019/06/06 20:23:01 by japarbs          ###   ########.fr       */
+/*   Updated: 2019/06/07 17:37:25 by japarbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#define BUFFCHECK	{*(buffer) = count; break;}
 
 /*
 **	Reads line from FD and joins the current buff and current index of **line,
 **	allocating the chars as it reads, allowing for dynamic string len.
 **	eof (Bollean) is a signal for End Of File. If the read buff is less
-**	than BUFF_SIZE then eof is triggered and will return 1 (for complete read).
+**	than BUFF_SIZE then eof is triggered and will return 1 (complete read).
 **	cbl = Current Buffer Length.
 */
 
-int		read_line(int fd, char **line, size_t line_index)
+int		read_line(int fd, char **line, char **fdarr)
 {
-	size_t			cbl;
-	size_t			i;
+	int				bres;
 	int				eof;
-	char			buffer[BUFF_SIZE];
+	char			buffer[BUFF_SIZE + 1];
+	char			*tmp;
 
-	i = 0;
 	eof = 0;
-	cbl = 0;
-	while (read(fd, buffer, BUFF_SIZE))
+	while ((bres = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
-		if (!buffer[BUFF_SIZE - 1])
+		buffer[bres] = '\0'
+		if (!bres)
 			eof = 1;
-		cbl = ft_findsubstrlen(buffer, '\n');
 		if (!(line[line_index] = ft_strjoin(line[line_index], buffer)))
 			return (-1);
+		if (ft_strdlen(buffer, '\n') < BUFF_SIZE)
+			break;
 	}
 	if (!eof)
 		return (0);
@@ -45,21 +44,18 @@ int		read_line(int fd, char **line, size_t line_index)
 }
 
 /*
-**	Main function, runs checks and keeps track of line positon & file positon
+**	'home' function, runs return checks and keeps track of current  & file positon
+**	MAX_FD is the total ammount of FDs get_next_line can handle (defined in .h)
 */
 
 int		get_next_line(const int fd, char **line)
 {
-	static size_t	line_index;
+	static char	*fdarr[MAX_FD];
 	int				rval;
 
-	if (fd < 0 || !line)
-		return (-1);
-	rval = read_line(fd, line, line_index);
-	if (rval == -1)
+	if (fd == -1 || !line || (rval = read_line(fd, line, fdarr) == -1))
 		return (-1);
 	if (rval == 0)
 		return (0);
-	line_index = 0;
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: japarbs <japarbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 23:43:33 by japarbs           #+#    #+#             */
-/*   Updated: 2019/06/06 17:35:31 by japarbs          ###   ########.fr       */
+/*   Updated: 2019/06/07 16:25:14 by japarbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 32
+# define MAX_FD 9001
 
 # include "libft/libft.h"
 
