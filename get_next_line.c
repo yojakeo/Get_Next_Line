9a743023525ac67f020210a037929200655447e7
Jared Parbs <japarbs@e1z2r3p40.42.us.org>
2019-06-12 04:10:33 -0700

    Updated comments, function name and added limits.h for OPEN_MAX macro

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,29 @@
 /*   By: japarbs <japarbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 16:18:40 by japarbs           #+#    #+#             */
-/*   Updated: 2019/06/12 01:58:47 by japarbs          ###   ########.fr       */
+/*   Updated: 2019/06/12 04:09:30 by japarbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 /*
-**	Reads line from FD and joins the current buff and current index of **line,
-**	allocating the chars as it reads, allowing for dynamic string len.
-**	bres (buffer result) is used to create a NULL terminator in buffer.
-**	bres is also used as an EOF indicator, if bres = 0 then EOF is true.
-**	lenres (length result) is used as an \n check and is used for the
-**	length limit in ft_strsub to push to current string of **line.
+**	Process_line takes the fd's fdarr char string and finds the index of \n.
+**	this index is stored in lenres (length result) 
+**	Using that len as how long the line is and pushing all till \n to **line.
+**	after **line is done, the leftover chars are stored in fdarr[fd]
+**	to be used in the next recall of get_next_line.
+**	statsize (static size) is the messured size of the extra bytes.
+**	bres is imported from get_next_line for the EOF check as stated bellow. 
 */
 
-int		read_line(int fd, char **line, char **fdarr, int bres)
+int		process_line(int fd, char **line, char **fdarr, int bres)
 {
 	size_t			lenres;
 	size_t			statsize;
 	char			*tmp;
 
-	if (!bres && !fdarr[fd])
+	if (!bres)
 	{
 		free(fdarr[fd]);
 		fdarr[fd] = NULL;
@@ -48,25 +49,32 @@ int		read_line(int fd, char **line, char **fdarr, int bres)
 
 /*
 **	Runs return checks and keeps track of FDs and handles return values.
-**	fdarr is an array of pointers, FD is used as an index and the pointer
-**	is a holder of the current read string that will later be pushed to **line,
-**	this allows the function to handle mutiple FDs at once. fdarr also is used
-**	as storage between lines after each recall, saving the unused chars for the
-**	next **line 
-**	MAX_FD is the total ammount of FDs get_next_line can handle (defined in .h)
+**	fdarr is an hash index, the pointer is a holder of the current read string
+**	that will later be pushed to **line, this allows the function to handle
+**	mutiple FDs at once. fdarr also is used as storage between lines
+**	after each recall, saving the unused chars for the next string in **line.
+**	OPEN_MAX (from limits.h) is the total ammount of FDs next_line can handle.
+**	...
+**	Function Logic
+**	Reads line from FD and joins the current buff and remaining bytes,
+**	allocating the chars as it reads, allowing for dynamic string len.
+**	bres (buffer result) is used to create a NULL terminator in buffer.
+**	bres is also used as an EOF indicator, if bres = 0 then EOF is true.
+**	*tmp string is nessesary to free fdarr[fd] to assure there are no leaks.
+**	if a \n is found the while loop breaks and enters phase 2 process_line.
 */
 
 int		get_next_line(const int fd, char **line)
 {
-	static char		*fdarr[MAX_FD];
+	static char		*fdarr[OPEN_MAX];
 	int				bres;
 	char			buffer[BUFF_SIZE + 1];
 	char			*tmp;
 
+	if (!fd || fd <= -1 || fd > OPEN_MAX || !line)
+		return (-1);
 	if (!(fdarr[fd]))
 		fdarr[fd] = ft_strnew(0);
-	if (fd == -1 || !line)
-		return (-1);
 	while ((bres = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
 		buffer[bres] = '\0';
@@ -79,5 +87,5 @@ int		get_next_line(const int fd, char **line)
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
-	return(read_line(fd, line, fdarr, bres));
+	return(process_line(fd, line, fdarr, bres));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: japarbs <japarbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 23:43:33 by japarbs           #+#    #+#             */
-/*   Updated: 2019/06/12 01:41:16 by japarbs          ###   ########.fr       */
+/*   Updated: 2019/06/12 03:39:21 by japarbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@
 # define MAX_FD 9001
 
 # include "libft/libft.h"
+# include <limits.h>
 
-int		read_line(int fd, char **line, char **fdarr, int bres);
+int		process_line(int fd, char **line, char **fdarr, int bres);
 int		get_next_line(const int fd, char **line);
 
 #endif
